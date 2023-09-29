f776da4ecb11318564221b6bfc3aee3d0d8ba019
knacko <geekbike@hotmail.com>
2023-09-29 15:37:17 -0600

    docs: added function defs

--- downsample.py
+++ downsample.py
@@ -4,15 +4,28 @@ from pathlib import Path
 from alive_progress import alive_bar
 
 def isGZ(filepath):
+    """Check if file is GZIP'd
+    :param filepath: File to check
+    :return: bool
+    """    
     with open(filepath, 'rb') as f:
         return f.read(2) == b'\x1f\x8b'
 
 def getReads(file):
+    """Counts number of reads in a file via zcat
+    :param file: File to check
+    :return: Number of reads in the file
+    """    
     zcat = subprocess.Popen(["zcat",file], stdout=subprocess.PIPE, text=True)
     numseqs = int(subprocess.run(["wc", "-l"], stdin=zcat.stdout, capture_output=True).stdout)
     return int(numseqs / 4)
     
 def seqtk_sample(file, output, depth):
+    """Subsamples a FASTQ/FASTA file via seqtk sample. Can be GZIP'd.
+    :param file: Path to file to subsample
+    :param output: The output directory
+    :param depth: The depth to sample
+    """    
     # seqtk sample [file] [depth] > [output]
     with open(output, "w") as outfile:
         if (isGZ(file)):
@@ -22,6 +35,11 @@ def seqtk_sample(file, output, depth):
             subprocess.run(["seqtk","sample",str(file),str(depth)], stdout=outfile)
 
 def samtools_view(file, output, depth):
+    """Subsamples a SAM/BAM file via samtools view.
+    :param file: Path to file to subsample
+    :param output: The output directory
+    :param depth: The depth to sample
+    """    
     #samtools view -s [proportion] -b [file] > [output]
     if (depth < 1): depth = depth + random.randint(1,100000) # For samtools; -s is INT.FRAC, where INT is the seed.
     if (Path(file).suffix != ".bam"): 
@@ -30,6 +48,15 @@ def samtools_view(file, output, depth):
         subprocess.run(["samtools","view","-s",depth,"-b","-o",output,file])
 
 def downsample(files, output, regex, depths = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], random = True, verbose = True):
+    """Downsample files at specified proportions. 
+
+    :param files: _description_
+    :param output: _description_
+    :param regex: _description_
+    :param depths: _description_, defaults to [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    :param random: Should the reads be random or sequential. If sequential, all reads will be subsampled from the previous subsampled file. E.g., for depths = [0.7,0.8,0.9], the 0.9 depth is subsampled from the input file, the 0.8 depth is subsampled from the 0.9 depth output, and the 0.7 is subsampled from the 0.8 depth file. defaults to True
+    :param verbose: _description_, defaults to True
+    """    
     files = files if isinstance(files, list) else [files]
     depths = depths if isinstance(depths, list) else [depths]
     
