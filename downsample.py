8e9fc64309c1bd7166ee222d17725f768ecc3e60
knacko <geekbike@hotmail.com>
2023-10-12 10:22:38 -0600

    feat: added argparser and additional flags

--- downsample.py
+++ downsample.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import subprocess, re, shutil, os, random
+import subprocess, re, shutil, os, random, argparse
 from pathlib import Path
 from alive_progress import alive_bar
 
@@ -47,15 +47,16 @@ def samtools_view(file, output, depth):
     else:
         subprocess.run(["samtools","view","-s",depth,"-b","-o",output,file])
 
-def downsample(files, output, regex, depths = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], random = True, verbose = True):
+def downsample(files, output, regex, depths = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], random = True, includeOriginal = True, verbose = True):
     """Downsample files at specified proportions. 
 
-    :param files: _description_
-    :param output: _description_
-    :param regex: _description_
-    :param depths: _description_, defaults to [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    :param files: Files to downsample, seperated by a space. E.g., `--files file1.fastq file2.fastq`.
+    :param output: Path to the output folder
+    :param regex: Regex for modifying the file name to include the downsample proportion. E.g., for `F1_S1_L001_R1_001.fastq.gz` and `0.5` depth, `regex = '(_S\\d*)'` would produce `F1_S1_0.5_L001_R1_001.fastq.gz`.
+    :param depths: Depth as represented as propotion of the original sample, defaults to [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9].
     :param random: Should the reads be random or sequential. If sequential, all reads will be subsampled from the previous subsampled file. E.g., for depths = [0.7,0.8,0.9], the 0.9 depth is subsampled from the input file, the 0.8 depth is subsampled from the 0.9 depth output, and the 0.7 is subsampled from the 0.8 depth file. defaults to True
-    :param verbose: _description_, defaults to True
+    :param includeOriginal: Include the original sample? Will be labelled with proportion of 1.0., defaults to True
+    :param verbose: Be chatty, defaults to True
     """    
     files = files if isinstance(files, list) else [files]
     depths = depths if isinstance(depths, list) else [depths]
@@ -70,7 +71,7 @@ def downsample(files, output, regex, depths = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7
     if not random: 
         for i in reversed(range(1,len(depths))): recursiveDepths.update({depths[i]: depths[i]/depths[i-1]})
     
-    with alive_bar(total = len(files)*(len(depths)+1), title="Downsampling files....", unknown="dots_waves", disable = not verbose) as bar:         
+    with alive_bar(total = len(files)*(len(depths)+int(includeOriginal)), title="Downsampling files....", unknown="dots_waves", disable = not verbose) as bar:         
         for file in files:
             f = Path(file)
             lastout = None
@@ -78,26 +79,28 @@ def downsample(files, output, regex, depths = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7
                 out = Path(output,re.sub(regex, fr'\1_{depth}', f.stem) + f.suffix)
                 if random or lastout is None:
                     seqtk_sample(file, out, depth)
-                    print(f"file: {file}\nout: {out}\ndepth: {depth}\n")
+                    # print(f"file: {file}\nout: {out}\ndepth: {depth}\n")
                 else:
                     seqtk_sample(lastout, out, recursiveDepths.get(depth))
-                    print(f"file: {lastout}\nout: {out}\ndepth: {recursiveDepths.get(depth)}\n")
+                    # print(f"file: {lastout}\nout: {out}\ndepth: {recursiveDepths.get(depth)}\n")
                 lastout = out
                 bar()
-            f = Path(output,re.sub(regex, fr'\1_1.0', f.stem) + f.suffix)
-            shutil.copyfile(file, f)
-            bar()
+            if (includeOriginal):
+                f = Path(output,re.sub(regex, fr'\1_1.0', f.stem) + f.suffix)
+                shutil.copyfile(file, f)
+                bar()            
 
-# file1 = "/nfs/Genomics_DEV/projects/alindsay/Projects/downsampler/F1_S1_L001_R1_001.fastq.gz"
-# file2 = "/nfs/Genomics_DEV/projects/alindsay/Projects/downsampler/F2_S2_L002_R2_002.fastq.gz"
-# output = "/nfs/Genomics_DEV/projects/alindsay/Projects/downsampler/"
-regex = '(_S\\d*)'
-depths = [0.5,0.1,0.05,0.01,0.005,0.001]
-# downsample(file1, output, regex, random = True)
-# downsample(file2, output, regex, random = False)
+def main(args):
+    downsample(args.files, args.output, args.regex, args.depths, args.random, args.includeOriginal, args.verbose)
 
-
-files = "/nfs/APL_Genomics/scratch/230927_N_I_008_WW/230927_MN01658_0129_A000H5NFKW/Alignment_1/20230928_065543/Fastq/"
-files = [os.path.abspath(os.path.join(files, p)) for p in os.listdir(files)]
-downsample(files, "/nfs/APL_Genomics/scratch/230927_N_I_008_WW/230927_MN01658_0129_A000H5NFKW/Alignment_1/20230928_065543/Fastq_rand/", regex, depths = depths, random = True)
-downsample(files, "/nfs/APL_Genomics/scratch/230927_N_I_008_WW/230927_MN01658_0129_A000H5NFKW/Alignment_1/20230928_065543/Fastq_seq/", regex, depths = depths, random = False)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--files', nargs='+', required=True, help='Files to downsample, seperated by a space. E.g., `--files file1.fastq file2.fastq`.')
+    parser.add_argument('-o','--output', required=True, help="Path to the output folder")
+    parser.add_argument('-r', '--regex', required=True, help="Regex for modifying the file name to include the downsample proportion. E.g., for `F1_S1_L001_R1_001.fastq.gz` and `0.5` depth, `regex = '(_S\\d*)'` would produce `F1_S1_0.5_L001_R1_001.fastq.gz`.")
+    parser.add_argument('-d', '--depths', nargs='+', default = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], help='The proportional depth to sequence. E.g., `0.5` would retain 50%% of reads, which generally would approximate 50%% depth.\nDefault is: 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9')
+    parser.add_argument('-x', '--random', action='store_true', help='Should the reads be random or sequential? If sequential, all reads will be subsampled from the previous subsampled file. E.g., for depths = [0.7,0.8,0.9], the 0.9 depth is subsampled from the input file, the 0.8 depth is subsampled from the 0.9 depth output, and the 0.7 is subsampled from the 0.8 depth file.')
+    parser.add_argument('-i', '--includeOriginal', action='store_true', help='Include the original sample? Will be labelled with proportion of 1.0.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Be chatty?')
+    args = parser.parse_args()
+    main(args)
